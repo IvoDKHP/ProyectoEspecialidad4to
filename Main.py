478400414ec2feffbcf963cfb856c45ebadba609
Alfa-rid <sarsurvillalonalfarid@gmail.com>
2024-11-20 23:04:10 -0300

    cambio_3

--- Main.py
+++ Main.py
@@ -1,22 +1,26 @@
 import sys
 import json
+import numpy as np
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import matplotlib.pyplot as plt
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 import Ui_menu
 import Ui_configuracion
-import Ui_estadisticas
+from Ui_estadisticas import*
 
 class MainWindow(QMainWindow):  # Main window class inherited from QMainWindow
     def __init__(self):  # Constructor
         super().__init__()
         self.ui_menu = Ui_menu.Ui_MainWindow()  # Main menu UI
         self.ui_config = Ui_configuracion.Ui_MainWindow()  # Configuration UI
-        self.ui_estadistic = Ui_estadisticas.Ui_MainWindow()
+        self.ui_estadistic = Ui_MainWindow()
         self.setup_menu()
         self.matriz_datos = []
-
     def setup_menu(self):
         # Set up the main menu interface
         self.ui_menu.setupUi(self)
@@ -72,8 +76,16 @@ class MainWindow(QMainWindow):  # Main window class inherited from QMainWindow
 
     def show_estadistic(self):
         # Return to the main menu
-        self.ui_estadistic.setupUi(self)
-        self.ui_estadistic.volver_boton.clicked.connect(self.show_menu)  
+        self.ui_estadistic.setupUi(self)  
+        self.grafica = Canvas_grafica()
+        self.grafica1 = Canvas_grafica2()
+        self.grafica2 = Canvas_grafica3()
+        self.grafica3 = Canvas_grafica4()
+        
+        self.ui_estadistic.grafica_uno.addWidget(self.grafica)
+        self.ui_estadistic.grafica_dos.addWidget(self.grafica1)
+        self.ui_estadistic.grafica_tres.addWidget(self.grafica2)
+        self.ui_estadistic.grafica_cuatro.addWidget(self.grafica3)
 
     def enviar_dato(self):
         # Obtener el valor del QSpinBox y añadirlo a la matriz
@@ -105,12 +117,94 @@ class MainWindow(QMainWindow):  # Main window class inherited from QMainWindow
         
         MainWindow.extraer_dato(ruta)
 
+    """def extraer_dato(ruta):
+        with open(ruta, "r", encoding="utf-8") as dato:
+            objeto = json.load(dato)  # Carga el JSON como un diccionario
+            matriz = objeto.get("matriz")  # Obtén el valor asociado a la clave "matriz"
+        print(matriz)
+        return matriz"""
+    
     def extraer_dato(ruta):
         with open(ruta, "r", encoding="utf-8") as dato:
             objeto = json.load(dato)  # Carga el JSON como un diccionario
             matriz = objeto.get("matriz")  # Obtén el valor asociado a la clave "matriz"
         print(matriz)
         return matriz
+    
+    class Dato_diario():
+        pass
+        
+class Canvas_grafica(FigureCanvas):
+    def __init__(self, parent=None):     
+        self.fig , self.ax = plt.subplots(1, dpi=100, figsize=(5, 5), 
+            sharey=True, facecolor='white')
+        super().__init__(self.fig) 
+
+        nombres = ['15', '25', '30', '35','40']
+        colores = ['red','red','red','red', 'red']
+        tamaño = [10, 15, 20, 25, 30]
+
+        self.ax.bar(nombres, tamaño, color = colores)
+        self.fig.suptitle('Grafica de Barras',size=9)
+
+
+class Canvas_grafica2(FigureCanvas):
+    def __init__(self, parent=None):     
+        self.fig , self.ax = plt.subplots(1,dpi=100, figsize=(5, 5), 
+            sharey=True, facecolor='white')
+        super().__init__(self.fig) 
+
+        nombres = ['Fresa', 'Piña', 'Lima', 'Uva']
+        colores = ['blue','yellow','aqua','fuchsia']
+        tamaño = [20, 26, 30, 24]
+        explotar = [0.05, 0.05, 0.05, 0.05] 
+
+        plt.title("Cantidad de Frutas Disponibles", color='black', size=9, family="DejaVu Sans")
+
+
+        self.ax.pie(tamaño, explode = explotar, labels = nombres, 
+            colors = colores,
+                autopct = '%1.0f%%', pctdistance = 0.6,
+                shadow=True, startangle=90, radius = 0.8, 
+                labeldistance=1.1)  
+        self.ax.axis('equal')
+
+
+class Canvas_grafica3(FigureCanvas):
+    def __init__(self, parent=None):     
+        self.fig , self.ax = plt.subplots(1, dpi=100, figsize=(5, 5), 
+            sharey=True, facecolor='white')
+        super().__init__(self.fig) 
+
+        self.fig.suptitle('Grafica de Datos',size=9)
+        np.random.seed(20)
+        y = np.random.randn(150).cumsum()
+
+        self.ax = plt.axes()
+        plt.plot(y, color='magenta')
+
+
+class Canvas_grafica4(FigureCanvas):
+    def __init__(self, parent=None):     
+        self.fig , self.ax = plt.subplots(1, dpi=100, figsize=(5, 5), 
+            sharey=True, facecolor='white')
+        super().__init__(self.fig) 
+
+        x = [1, 2, 3, 4, 5,6,7]
+        y1 = [1, 0, 1, 3, 2,4,3]
+        y2 = [0, 2, 2, 3, 4,6,5]
+        y3 = [3, 1, 3, 4, 2,7,6]
+
+        y = np.vstack([y1, y2, y3])
+
+        labels = ["Y1 ", "Y2", "Y3"]
+        color = ["orange","blue","green"]
+
+        self.ax.stackplot(x, y1, y2, y3, labels=labels,colors=color)
+        self.ax.legend(loc='upper left')
+        self.ax.stackplot(x, y)
+        self.fig.suptitle('Grafica Stackplot',size=9)
+
 
 if __name__ == "__main__":  # Entry point check
     app = QApplication(sys.argv)
